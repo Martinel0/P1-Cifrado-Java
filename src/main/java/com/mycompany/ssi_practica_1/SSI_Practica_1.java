68ee5076b012183928afb138d3efdfd3a97b5b34
Martín Fernández Cid <martin15allariz@gmail.com>
2025-10-06 19:54:46 +0200

    Esqueleto Proyecto

---
+++ src/main/java/com/mycompany/ssi_practica_1/SSI_Practica_1.java
@@ -0,0 +1,17 @@
+
+package com.mycompany.ssi_practica_1;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import java.security.*;
+import java.security.interfaces.*;
+import java.security.spec.*;
+import javax.crypto.*;
+import javax.crypto.interfaces.*;
+import javax.crypto.spec.*;
+public class SSI_Practica_1 {
+
+    public static void main(String[] args) {
+       
+        
+    }
+}
